41ad244a173fbca93758d62bd92aa4921630c4b7
Julian TRANI <73946832+JulianT31@users.noreply.github.com>
2022-10-13 11:08:13 +0200

    calcul position

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -24,7 +26,8 @@ def affichage_3_courbes(V, t0, t2, Te, CI=0):
     :param t0: debut intervalle
     :param t2: fin intervalle
     :param Te: période d'échantillonnage
-    :return: une numpy array à n valeurs
+    :param CI: condition initiale
+    :return:
     """
     size_vector_t, t_vector = get_discret(t0, t2, Te)
     t1 = t2 / 2  # attention peut etre division entiere
@@ -37,26 +40,61 @@ def affichage_3_courbes(V, t0, t2, Te, CI=0):
     # calcul de s(t) en fonction de la valeur de t
     # si t1 < t < t2 => -ab+V
     # si  0 > t < t1 => ab+V
-    speed_vector = np.array([a * t_vector[i] if t_vector[i] <= t1 else ((-1 * a) * (t_vector[i] - t1) + b) for i in
-                             range(int(size_vector_t))])
-
+    speed_vector = get_s_point_t(a, b, size_vector_t, t_vector)
     affichage_courbe(t_vector, speed_vector, "Courbe de .s(t) en fonction de t (en discret)", "bX")
 
     # calcul de s(t) (position)
-    pos_vector = np.array(
-        [(a / 2) * (t_vector[i] ** 2) + (b * t_vector[i]) + c if t_vector[i] <= t1 else
-         ((-1 * a) / 2) * (t_vector[i] ** 2 - t1) + b * (t_vector[i] - t1) + c for i in
-         range(int(size_vector_t))])
-
-    affichage_courbe(t_vector, pos_vector, "Courbe de s(t) en fonction de t (en discret)", "bX")
+    # pos_vector = get_s_t(a, b, c, size_vector_t, t_vector)
+    # affichage_courbe(t_vector, pos_vector, "Courbe de s(t) en fonction de t (en discret)", "bX")
 
     # calcul de s(t) (position)
-    ac_vector = np.array([a if t_vector[i] <= t1 else (-1 * a) for i in
-                          range(int(size_vector_t))])
-
+    ac_vector = get_s_seconde_t(a, b, size_vector_t, t_vector)
     affichage_courbe(t_vector, ac_vector, "Courbe de s(t) seconde en fonction de t (en discret)", "bX")
 
+    pos_vector = aire(t_vector, speed_vector, V, t1)
+    affichage_courbe(t_vector, pos_vector,
+                     "Position en fonction du temps", "rX")
+
+    sans_nom(pos_vector, t_vector, (0, 0, 0), (1, 1, 1))
+
     plt.show()
+
+
+def get_s_t(a, b, c, size_vector_t, t_vector):
+    return np.array(
+        [(a / 2) * (t_vector[i] ** 2) + (b * t_vector[i]) + c if t_vector[i] <= t1 else
+         ((-1 * a) / 2) * (t_vector[i] ** 2) + b * (t_vector[i]) + c for i in
+         range(int(size_vector_t))])
+
+
+def get_s_point_t(a, b, size_vector_t, t_vector):
+    return np.array([a * t_vector[i] if t_vector[i] <= t1 else ((-1 * a) * (t_vector[i] - t1) + b) for i in
+                     range(int(size_vector_t))])
+
+
+def get_s_seconde_t(a, b, size_vector_t, t_vector):
+    return np.array([a if t_vector[i] <= t1 else (-1 * a) for i in
+                     range(int(size_vector_t))])
+
+
+def aire(t_vector, s_vector, V, t1):
+    size_vector_t = len(t_vector)
+
+    return np.array(
+        [(t_vector[i] * s_vector[i]) / 2 if t_vector[i] <= t1 else (t1 * V) - (
+                ((t1 * 2 - t_vector[i]) * s_vector[i]) / 2)
+         for i in
+         range(int(size_vector_t))])
+
+
+def sans_nom(s_vector, t_vector, A, B):
+    # (x, y, z)
+    l = math.sqrt((A[0] - B[0]) ** 2 + (A[1] - B[1]) ** 2 + (A[2] - B[2]) ** 2)
+
+    d = np.array(
+        [t_vector[i] * s_vector[i] for i in range(int(len(s_vector)))])
+
+    affichage_courbe(t_vector, d, "Position en fonction de la vitesse", "rX")
 
 
 def affichage_courbe(x, y, title="", style=""):
@@ -64,9 +102,11 @@ def affichage_courbe(x, y, title="", style=""):
     Affiche une courbe selon x et y passé en paramètre
     :param x: numpy array à N valeurs
     :param y: numpy array à N valeurs
+    :param title: titre de la courbe
     :param style: style de la courbe (par defaut ligne)
     :return:
     """
+
     plt.figure()
     plt.plot(x, y, style)
     plt.title(title)
@@ -82,4 +122,4 @@ if __name__ == '__main__':
     t2 = 2 * t1
     Te = 0.5  # Te != 0 (souvent entre 1 et 10 ms)
 
-    affichage_3_courbes(V, t0, t2, Te)
+    affichage_3_courbes(V, t0, t2, Te, CI=0)
