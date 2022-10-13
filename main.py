27bdb132db9c98773cbc3fac2873c537be665de4
Julian TRANI <73946832+JulianT31@users.noreply.github.com>
2022-10-13 08:55:15 +0200

    add 3 courbes

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def get_discret(t0, t2, Te):
     :param t0: debut intervalle
     :param t2: fin intervalle
     :param Te: période d'échantillonnage
-    :return: une numpy array à n valeurs t (selon l'échantillonnage => (t2 // Te) + 1) )
+    :return: tuple => la taille du vecteur et une numpy array à n valeurs t (selon l'échantillonnage => (t2 // Te) + 1) )
     """
     # size of the vector
     size_vector_t = (t2 // Te) + 1
@@ -17,7 +17,7 @@ def get_discret(t0, t2, Te):
     return size_vector_t, t_vector
 
 
-def creation_s_de_t(V, t0, t2, Te):
+def affichage_3_courbes(V, t0, t2, Te, CI=0):
     """
     Calcul et retourne une numpy array
     :param V: valeur en ordonnée du point N
@@ -27,22 +27,39 @@ def creation_s_de_t(V, t0, t2, Te):
     :return: une numpy array à n valeurs
     """
     size_vector_t, t_vector = get_discret(t0, t2, Te)
-    t1 = t2 / 2
+    t1 = t2 / 2  # attention peut etre division entiere
 
     # paramètre fonction affine
     a = (V / t1)
     b = V
+    c = CI
 
     # calcul de s(t) en fonction de la valeur de t
     # si t1 < t < t2 => -ab+V
     # si  0 > t < t1 => ab+V
-    s_vector = np.array([a * t_vector[i] if t_vector[i] <= t1 else ((-1 * a) * (t_vector[i] - t1) + b) for i in
-                         range(int(size_vector_t))])
+    speed_vector = np.array([a * t_vector[i] if t_vector[i] <= t1 else ((-1 * a) * (t_vector[i] - t1) + b) for i in
+                             range(int(size_vector_t))])
 
-    affichage_courbe(t_vector, s_vector, "bX")
-    return s_vector
+    affichage_courbe(t_vector, speed_vector, "Courbe de .s(t) en fonction de t (en discret)", "bX")
 
-def affichage_courbe(x, y, style=""):
+    # calcul de s(t) (position)
+    pos_vector = np.array(
+        [(a / 2) * (t_vector[i] ** 2) + (b * t_vector[i]) + c if t_vector[i] <= t1 else
+         ((-1 * a) / 2) * (t_vector[i] ** 2 - t1) + b * (t_vector[i] - t1) + c for i in
+         range(int(size_vector_t))])
+
+    affichage_courbe(t_vector, pos_vector, "Courbe de s(t) en fonction de t (en discret)", "bX")
+
+    # calcul de s(t) (position)
+    ac_vector = np.array([a if t_vector[i] <= t1 else (-1 * a) for i in
+                          range(int(size_vector_t))])
+
+    affichage_courbe(t_vector, ac_vector, "Courbe de s(t) seconde en fonction de t (en discret)", "bX")
+
+    plt.show()
+
+
+def affichage_courbe(x, y, title="", style=""):
     """
     Affiche une courbe selon x et y passé en paramètre
     :param x: numpy array à N valeurs
@@ -50,11 +67,11 @@ def affichage_courbe(x, y, style=""):
     :param style: style de la courbe (par defaut ligne)
     :return:
     """
+    plt.figure()
     plt.plot(x, y, style)
-    plt.title("Courbe de .s(t) en fonction de t (en discret)")
+    plt.title(title)
     plt.xlabel('t')
     plt.ylabel('s(t)')
-    plt.show()
 
 
 if __name__ == '__main__':
@@ -65,4 +82,4 @@ if __name__ == '__main__':
     t2 = 2 * t1
     Te = 0.5  # Te != 0 (souvent entre 1 et 10 ms)
 
-    creation_s_de_t(V, t0, t2, Te)
+    affichage_3_courbes(V, t0, t2, Te)
